be7fabb1ef7f1a7b9bdd6031ad697cbbda0fef7a
Nyaarium <project@nyaarium.com>
2026-03-09 04:24:13 -0700

    Fix for root users

--- post-start-base.sh
+++ post-start-base.sh
@@ -6,8 +6,8 @@ set -e
 
 
 # Remind user if git identity is not configured
-GIT_NAME=$(git config --global user.name 2>/dev/null)
-GIT_EMAIL=$(git config --global user.email 2>/dev/null)
+GIT_NAME=$(git config --global user.name 2>/dev/null || true)
+GIT_EMAIL=$(git config --global user.email 2>/dev/null || true)
 if [ -z "$GIT_NAME" ] || [ -z "$GIT_EMAIL" ]; then
 	echo ""
 	echo "Set your git identity with:"
